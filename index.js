9e37c1972991af9665dc4ce10e5607c144e61162
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-11-05 21:02:12 +0100

    changed 'promise' to 'asyncFunction'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const deasync = require('deasync');
 const util = require('util');
 
-module.exports.promise = function(fn) {
+module.exports.asyncFunction = function(fn) {
   let cbFunction = util.callbackify(fn);
   let deFunction = deasync(cbFunction);
   return deFunction;
@@ -13,10 +13,10 @@ module.exports.callback = function(fn) {
 }
 
 module.exports.call = {};
-module.exports.call.promise = function(fn, ...args) {
+module.exports.call.asyncFunction = function(fn, ...args) {
   let result = null;
   try {
-    let deFunction = module.exports.promise(fn);
+    let deFunction = module.exports.asyncFunction(fn);
     result = deFunction(...args);
   } catch (e) {
     console.error(e);
